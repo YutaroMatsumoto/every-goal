6381bd9e360fdb45818a3d5d66b68807399d35b4
yutaro_matsumoto <spurs03170629@gmail.com>
2025-03-16 12:33:09 +0900

    icon

---
+++ src/lib/components/ui/_icon/Icon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Icon as _Icon, icon, IconSize } from "./icon";
+  import type { IconColorType, IconSizeType, IconType } from "./icon"
+
+  type Props = {
+    type: IconType
+    color: IconColorType
+    size?: IconSizeType
+  }
+  let { type, color, size = "md" }: Props = $props()
+
+  const Icon = $derived(_Icon[type])
+  const iconSize = $derived(IconSize[size])
+  const iconClass = $derived(icon({ color }))
+</script>
+
+<Icon size={iconSize} class={iconClass} />
---
+++ src/lib/components/ui/_icon/icon.ts
@@ -0,0 +1,36 @@
+import { tv } from "tailwind-variants"
+import { IconSearch, IconCameraFilled, IconSend2, IconX, IconMenu2, IconArrowLeft, IconEyeFilled } from "@tabler/icons-svelte";
+import ClockHour_4 from "@tabler/icons-svelte/icons/clock-hour-4";
+
+export type IconSizeType = "sm" | "md" | "lg"
+export const IconSize: {[key in IconSizeType]: number} = {
+  sm: 16,
+  md: 24, // 仮
+  lg: 48
+}
+
+export type IconColorType = keyof typeof icon.variants.color;
+
+export const icon = tv({
+  variants: {
+    color: {
+      primary: "text-blue-600", // 仮
+      // secondary:
+      primaryLight: "text-blue-300",
+      disabled: "text-gray-400"
+    },
+  },
+})
+
+export const Icon = {
+  search: IconSearch,
+  clock: ClockHour_4,
+  camera: IconCameraFilled,
+  send: IconSend2,
+  close: IconX,
+  menu: IconMenu2,
+  arrowLeft: IconArrowLeft,
+  eye: IconEyeFilled
+} as const;
+
+export type IconType = keyof typeof Icon
