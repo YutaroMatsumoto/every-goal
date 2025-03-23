832579bad38434b6c2671f03b0b5560f1f69fc50
yutaro_matsumoto <spurs03170629@gmail.com>
2025-03-23 14:57:04 +0900

    update route const

--- src/lib/const/route.ts
+++ src/lib/const/route.ts
@@ -3,4 +3,8 @@ export const PUBLIC_PATHS = ['/login', '/signup', '/reset-password'];
 export const pathName = {
 	home: '/',
 	login: '/login',
+	my: '/my',
 } as const
+
+export type PathName = keyof typeof pathName
+export type PathValues = (typeof pathName)[PathName]
