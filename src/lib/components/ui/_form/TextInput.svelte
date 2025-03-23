c1a2b41207f522836508311bdfcfd847695c8255
yutaro_matsumoto <spurs03170629@gmail.com>
2025-03-23 15:12:42 +0900

    update TextInput style

--- src/lib/components/ui/_form/TextInput.svelte
+++ src/lib/components/ui/_form/TextInput.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <input
-  class="w-full h-12 px-4 py-2 border border-gray-300 rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+  class="w-full h-10 px-4 py-2 border border-gray-300 rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
   type="text"
   bind:value
   {placeholder}
