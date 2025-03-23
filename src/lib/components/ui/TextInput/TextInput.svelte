81977a5ecd30e7a13d1adcb3da5d3bebfb2c4185
yutaro_matsumoto <spurs03170629@gmail.com>
2025-03-23 09:40:54 +0900

    add TextInput

---
+++ src/lib/components/ui/TextInput/TextInput.svelte
@@ -0,0 +1,24 @@
+<!-- MEMO: 初期はformはそんなに多くないのでシンプルな作りにする -->
+<script lang="ts">
+  type Props = {
+    value: string;
+    placeholder?: string;
+    keydownFunction?: () => void;
+  }
+  let { value = $bindable(), placeholder, keydownFunction }: Props = $props();
+
+  const onkeydown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter' &&keydownFunction) {
+      keydownFunction();
+    }
+  }
+
+</script>
+
+<input
+  type="text"
+  bind:value
+  {placeholder}
+  class="flex-1 px-4 py-2 border border-gray-300 rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+  {onkeydown}
+/>
