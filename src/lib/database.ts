f4a92677a516aa6308618a3570e67bef472d0b11
yutaro_matsumoto <spurs03170629@gmail.com>
2025-03-26 00:03:46 +0900

    update users table

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -14,22 +14,22 @@ export type Database = {
           avatar_url: string | null
           created_at: string
           display_name: string
-          id: string
           updated_at: string
+          user_id: string
         }
         Insert: {
           avatar_url?: string | null
           created_at?: string
           display_name: string
-          id?: string
           updated_at?: string
+          user_id?: string
         }
         Update: {
           avatar_url?: string | null
           created_at?: string
           display_name?: string
-          id?: string
           updated_at?: string
+          user_id?: string
         }
         Relationships: []
       }
